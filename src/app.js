7e8e78c190fcf44e890b1d0acd2b3280d09f68b1
Azul MA <azulma2826@gmail.com>
2025-08-28 22:42:30 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
 import "bootstrap";
 import "./style.css";
 
+  let pronoun = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
+  let extension = [".com", ".net"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+for ( let p = 0; p<pronoun.lenght; p++) {
+  for (let a = 0; a<adj.lenght; a++) {
+    for (let n = 0; n< noun.lenght; n++) {
+      for (let e = 0; e< extension.lenght; e++) {
+          console.log( pronoun[p] + adj[a] + noun[n] + extension[e]);
+      }
+    }
+  }
+}
